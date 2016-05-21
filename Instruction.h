973aff68b6751216debbcd8abb4afe19d5e60cb2
Eamon <olivee3@rpi.edu>
2016-05-20 20:39:19 -0400

    Attempted to flesh out the Instruction.h.

--- Instruction.h
+++ Instruction.h
@@ -1,9 +1,12 @@
 #ifndef __instruction_h_
 #define __instruction_h_
 
+#include "Board.h"
+
 class Instruction {
 	public:
 		Instruction();
+		virtual void operator(Waldo parent, Board board) {}
 };
 
 #endif
