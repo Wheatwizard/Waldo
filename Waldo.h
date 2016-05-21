84657edf96e80bfee2f74ccf70337876020b011e
1000000000 <lthyde@gmail.com>
2016-05-20 20:41:16 -0400

    Added a bit to Waldo

--- Waldo.h
+++ Waldo.h
@@ -9,16 +9,22 @@ enum direction {NO_DIRECTION = 0, LEFT, UP, RIGHT, DOWN};
 class Waldo {
 	public:
 		Waldo(std::string n, unsigned row, unsigned col) :
-			name(n), holding(NULL), facing(NO_DIRECTION), paused(false)
+			name(n), holding(NULL), facing(NO_DIRECTION), paused(false), coords([row, col])
 		{
-			coords = [row,col];
+			static unsigned id = 0;
+			priority = id++;
 		}
+
+		unsigned getPriority() const { return priority; }
+
+
 	private:
 		std::string name;
 		Atom* holding;
 		direction facing;
 		bool paused;
-		unsigned coords;
+		unsigned* coords;
+		unsigned priority;
 };
 
 #endif
