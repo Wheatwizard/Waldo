918e529a9c9f0e02cae734eef9480b65c811e238
wheatwizard <olivee3@rpi.edu>
2016-06-02 13:05:56 -0400

    Built a version of the Instruction class

--- Instruction.h
+++ Instruction.h
@@ -5,9 +5,37 @@
 
 class Instruction {
 	public:
-		Instruction() {};
-	private:
+		//CONSTRUCTORS
+		Instruction() :
+		variety(NO_INSTRUCTION), facing(NO_DIRECTION), rotation(true) {};
 		
+		Instruction(InstructionClass v) :
+		variety(v), facing(NO_DIRECTION), rotation(true) {};
+		
+		Instruction(InstructionClass v, Direction d) :
+		variety(v), facing(d), rotation(true) {};
+		
+		Instruction(InstructionClass v, bool r) :
+		variety(v), facing(NO_DIRECTION), rotation(r) {};
+		
+		Instruction(InstructionClass v, Direction d, bool r) :
+		variety(v), facing(d), rotation(r) {};
+		
+		//ACCESSORS
+		InstructionClass getVariety()   const { return variety; }
+		Direction        getDirection() const { return facing;  }
+		bool             getRotation()  const { return rotation;}
+		
+		//MODIFIERS
+		void setVariety(  InstructionClass v) { variety  = v; }
+		void setFacing(   Direction        d) { facing   = d; }
+		void setRotation( bool             r) { rotation = r; }
+	
+	private:
+		InstructionClass variety;
+		Direction facing;
+		//true is counterclockwise
+		bool rotation;
 };
 
 #endif
