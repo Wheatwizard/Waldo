d2a30fdfe253dd0b82e78d1fc794327d6cb283c3
wheatwizard <olivee3@rpi.edu>
2016-06-10 14:13:31 -0400

    Environments now advance sinks

--- Environment.cpp
+++ Environment.cpp
@@ -4,6 +4,9 @@ void Environment::advance() {
 	for (unsigned i = 0; i < reactors.size(); ++i) {
 		reactors[i].advance();
 	}
+	for (unsigned i = 0; i < sinks.size(); ++i) {
+		sinks[i].advance();
+	}
 }
 
 void Environment::addBoard(unsigned width, unsigned height, unsigned num_waldos) {
