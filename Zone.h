cfed034d6a5d03afca5b10d77bb25a9bf6201a84
Eamon <olivee3@rpi.edu>
2016-06-14 19:40:32 -0400

    Started work on zones

---
+++ Zone.h
@@ -0,0 +1,21 @@
+#ifndef __zone_h_
+#define __zone_h_
+
+#include <string>
+
+class Zone {
+	public:
+		Zone();
+		Zone(std::string n; unsigned w; unsigned h) : name(n), width(w), height(h);
+		//ACESSORS
+		std::string getName()    const { return name;   }
+		unsigned    getWidth()   const { return width;  }
+		unsigned    get Height() const { return height; }
+	private:
+		std::string name;
+		unsigned width;
+		unsigned height;
+		Pipe* link;
+}
+
+#endif
